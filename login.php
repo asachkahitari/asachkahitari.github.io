7558bec7fb40ae6947af0b07c2634d3a116283ed
Shrinidhi <shrinidhi.21810366@viit.ac.in>
2020-10-02 11:01:59 +0530

    login page with css

---
+++ login.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Admin Panel</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
+	<!-- Custom styles for this template-->
+  <link href="login.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-6 col-md-6">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Login Here!</h1>
+                                    </div>
+                                    <form class="user" action="#" method="POST">
+                                        <div class="form-group">
+                                            <input type="text" name="usernamee" class="form-control form-control-user" placeholder="Enter username...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="passwordd" class="form-control form-control-user" placeholder="Password">
+                                        </div>
+                                        <button type="submit" name="login_btn" class="btn btn-primary btn-user btn-block"> Login </button>
+                                        <hr>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>S
+            </div>
+        </div>
+    </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </body>
+ </html>
